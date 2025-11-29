af4be7cfd90b0473330b88b9ea37da3a6d108272
Mouli <ursmouli@gmail.com>
2025-11-29 16:47:26 +0530

    products added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,12 @@ import { Services } from './pages/services/services';
 import { Clients } from './pages/clients/clients';
 import { Products } from './pages/products/products';
 import { MainLayout } from './layout/main-layout/main-layout';
+import { Books } from './pages/books/books';
+import { WeddingCards } from './pages/wedding-cards/wedding-cards';
+import { InvitationCards } from './pages/invitation-cards/invitation-cards';
+import { Calendars } from './pages/calendars/calendars';
+import { Posters } from './pages/posters/posters';
+import { Stationary } from './pages/stationary/stationary';
 
 
 export const routes: Routes = [
@@ -19,11 +25,14 @@ export const routes: Routes = [
             { path: 'about', component: About, pathMatch: 'full'},
             { path: 'services', component: Services, pathMatch: 'full'},
             { path: 'clients', component: Clients, pathMatch: 'full'},
-            { 
-                path: 'products', 
-                component: Products, 
-                pathMatch: 'full'
-            },
+            { path: 'products', component: Products, pathMatch: 'full'},
+            { path: 'products/brouchers', component: Books, pathMatch: 'full'},
+            { path: 'products/books', component: Books, pathMatch: 'full'},
+            { path: 'products/wedding-cards', component: WeddingCards, pathMatch: 'full'},
+            { path: 'products/invitation-cards', component: InvitationCards, pathMatch: 'full'},
+            { path: 'products/calendars', component: Calendars, pathMatch: 'full'},
+            { path: 'products/stationary', component: Stationary, pathMatch: 'full'},
+            { path: 'products/posters', component: Posters, pathMatch: 'full'},
             { path: 'contact', component: Contact, pathMatch: 'full'},
         ]
     },
